d0fe1bfab704aa0b7bd0e8b1b537f08482661bb9
kingtrip3 <71353638+kingtrip3@users.noreply.github.com>
2021-04-29 17:42:34 -0400

    completed navbar and created home component

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCdJ03mZAvjZzWWcQZGeI4WR3ikzmC49AI",
+  authDomain: "clone-ea36f.firebaseapp.com",
+  projectId: "clone-ea36f",
+  storageBucket: "clone-ea36f.appspot.com",
+  messagingSenderId: "518223330899",
+  appId: "1:518223330899:web:d049aea6412997e958d0a6",
+  measurementId: "G-9MJXRB1QK3"
+};
